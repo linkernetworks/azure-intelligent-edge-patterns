fc7d8f7a1360409b2b7f2f003202220e1bdea7d7
ronpai <ronpai@MacBook-Pro.local>
2020-08-31 17:31:45 +0800

    add parts condition

--- factory-ai-vision/EdgeSolution/modules/InferenceModule/main.py
+++ factory-ai-vision/EdgeSolution/modules/InferenceModule/main.py
@@ -1003,8 +1003,8 @@ def gen_edge():
     predictions = onnx.last_prediction
     for prediction in predictions:
         tag = prediction['tagName']
-        # if tag not in onnx.parts:
-        #     continue
+        if tag not in onnx.parts:
+            continue
 
         if onnx.has_aoi:
             # for aoi_area in onnx.aoi_info:
