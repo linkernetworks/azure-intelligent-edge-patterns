a4edcc0da573a4a19dc692a78bf3a85367ab3a87
goatwu1993 <peteeelol@gmail.com>
2020-09-25 17:14:06 +0800

    Make tests faster.

--- factory-ai-vision/EdgeSolution/modules/WebModule/backend_v2/vision_on_edge/azure_part_detections/tests/conftest.py
+++ factory-ai-vision/EdgeSolution/modules/WebModule/backend_v2/vision_on_edge/azure_part_detections/tests/conftest.py
@@ -5,25 +5,22 @@ from unittest import mock
 
 import pytest
 
+from ...azure_projects.models import Project
 from ...azure_settings.models import Setting
+
+
+class MockedProject:
+    def __init__(self):
+        self.name = "Mocked Project Name"
 
 
 @pytest.fixture(scope="function", autouse=True)
 def mock_validate(monkeypatch):
     monkeypatch.setattr(Setting, "validate", mock.MagicMock(return_value=True))
-
-
-@pytest.fixture(scope="function", autouse=True)
-def mock_get_domain_id(monkeypatch):
     monkeypatch.setattr(
         Setting, "get_domain_id", mock.MagicMock(return_value="Fake_id")
     )
 
-
-# @pytest.fixture(scope="module")
-# def mock_validate(monkeypatch):
-# class FakeProject:
-# def __init__(self):
-# self.name = "Fake Project"
-
-# monkeypatch.setattr(Setting, "validate", True)
+    monkeypatch.setattr(
+        Project, "get_project_obj", mock.MagicMock(return_value=MockedProject())
+    )
--- factory-ai-vision/EdgeSolution/modules/WebModule/backend_v2/vision_on_edge/azure_parts/tests/conftest.py
+++ factory-ai-vision/EdgeSolution/modules/WebModule/backend_v2/vision_on_edge/azure_parts/tests/conftest.py
@@ -5,7 +5,13 @@ from unittest import mock
 
 import pytest
 
+from ...azure_projects.models import Project
 from ...azure_settings.models import Setting
+
+
+class MockedProject:
+    def __init__(self):
+        self.name = "Mocked Project Name"
 
 
 @pytest.fixture(scope="function", autouse=True)
@@ -20,10 +26,8 @@ def mock_get_domain_id(monkeypatch):
     )
 
 
-# @pytest.fixture(scope="module")
-# def mock_validate(monkeypatch):
-# class FakeProject:
-# def __init__(self):
-# self.name = "Fake Project"
-
-# monkeypatch.setattr(Setting, "validate", True)
+@pytest.fixture(scope="function", autouse=True)
+def mock_get_project_obj(monkeypatch):
+    monkeypatch.setattr(
+        Project, "get_project_obj", mock.MagicMock(return_value=MockedProject())
+    )
